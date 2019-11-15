f922acb6c0d94382c8d8310938a97ce07dfe4dd2
Sheila Wendler <seeknewaddrs@gmail.com>
2019-11-15 08:16:37 +0800

    all: pypi mirrors

---
+++ pypi.py
@@ -0,0 +1,23 @@
+'''
+    PyPi 更换为国内镜像源
+'''
+
+import sys
+import os
+
+mirrors = '''\
+[global]
+timeout = 60
+index-url = https://mirrors.ustc.edu.cn/pypi/web/simple/
+extra-index-url = https://mirrors.tuna.tsinghua.edu.cn/pypi/web/simple/\
+'''
+
+user = os.path.expanduser('~')
+if sys.platform == 'win32':
+    os.mkdir(f"{user}/AppData/Roaming/pip")
+    with open(f"{user}/AppData/Roaming/pip/pip.ini", 'w', encoding='utf-8') as fp:
+        fp.write(mirrors)
+elif sys.platform == 'linux' or sys.platform == 'darwin':
+    os.mkdir(f"{user}/.pip")
+    with open(f"{user}/.pip/pip.conf", 'w', encoding='utf-8') as fp:
+        fp.write(mirrors)
