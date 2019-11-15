677d4642710dafd135f36b207675ade769dc324b
Sheila Wendler <seeknewaddrs@gmail.com>
2019-11-15 08:16:42 +0800

    all: kali linux mirrors

---
+++ kali.py
@@ -0,0 +1,24 @@
+'''
+    Kali Linux 更换为国内镜像源
+'''
+
+sources_list = '''\
+#中科大
+deb http://mirrors.ustc.edu.cn/kali kali-rolling main non-free contrib
+deb-src http://mirrors.ustc.edu.cn/kali kali-rolling main non-free contrib
+
+#阿里云
+deb http://mirrors.aliyun.com/kali kali-rolling main non-free contrib
+deb-src http://mirrors.aliyun.com/kali kali-rolling main non-free contrib
+
+#清华大学
+deb http://mirrors.tuna.tsinghua.edu.cn/kali kali-rolling main contrib non-free
+deb-src https://mirrors.tuna.tsinghua.edu.cn/kali kali-rolling main contrib non-free
+
+#浙大
+deb http://mirrors.zju.edu.cn/kali kali-rolling main contrib non-free
+deb-src http://mirrors.zju.edu.cn/kali kali-rolling main contrib non-free\
+'''
+
+with open("/etc/apt/sources.list", 'w', encoding='utf-8') as fp:
+    fp.write(sources_list)
